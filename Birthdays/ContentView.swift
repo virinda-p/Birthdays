895d73f4dc64b3b6d77a26b7f31f1ea81d889136
Virinda Popli <poplivirinda0@gmail.com>
2025-07-11 11:34:17 -0400

    finished step 3

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    //friends array
+    @State private var friends: [Friend] = [
+        Friend(name: "Virinda", birthday: .now),
+        Friend(name: "Hailey", birthday: .now)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                        }
+                    Button("Save"){
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
-        .padding()
     }
 }
 
---
+++ Birthdays/Friend.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
+
